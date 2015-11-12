783dc6a9725241bae5d214df8b6346d18c0042a7
NinhDo <doquininh@gmail.com>
2015-11-12 15:26:24 +0100

    about stuff

--- projectcottonfist/scripts/expand.js
+++ projectcottonfist/scripts/expand.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("click", isExpandable);
     window.addEventListener("hashchange", expandBoxFromDropdown);
+    expandBoxFromDropdown();
 });
 
 function isExpandable() {
