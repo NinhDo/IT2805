f3ebb767c2dd6b93da9c73418462f52211f8d88e
NinhDo <doquininh@gmail.com>
2015-11-13 12:49:53 +0100

    filter.js filter fix

--- projectcottonfist/scripts/filter.js
+++ projectcottonfist/scripts/filter.js
@@ -34,7 +34,14 @@ function search() {
         hideAll(content);
         for (i = 0; i < content.childElementCount; i++) {
             for (var x = 0; x < content.children[i].childElementCount; x++) {
-                if (content.children[i].children[x].innerHTML.toLowerCase().indexOf(text.toLowerCase()) >= 0) {
+                if (x == 1) {
+                    for (var y = 0; y < content.children[i].children[1].childElementCount; y++) {
+                        if (content.children[i].children[1].children[y].innerHTML.toLowerCase().indexOf(text.toLowerCase()) >= 0) {
+                            content.children[i].classList.toggle("hidden");
+                            break;
+                        }
+                    }
+                } else if (content.children[i].children[x].innerHTML.toLowerCase().indexOf(text.toLowerCase()) >= 0) {
                     content.children[i].classList.toggle("hidden");
                     break;
                 }
